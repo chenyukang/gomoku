825ab4fd956db5d22e6241ea136a167dba632721
chenyukang <moorekang@gmail.com>
2021-02-04 15:17:54 +0800

    parse arguments

--- algo/src/main.rs
+++ algo/src/main.rs
@@ -1,3 +1,26 @@
+
+use clap::clap_app;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = clap_app!(myapp =>
+        (version: "0.1")
+        (author: "yukang <moorekang@gmail.com>")
+        (about: "Algo backend for Gomoku")
+        (@arg INPUT: +required "Current board of gomoku")
+        (@arg verbose: -v --verbose "Print version information verbosely")
+        (@arg depth: -d --depth +takes_value "The search depth for algo")        
+    ).get_matches();
+
+    let mut search_depth = 14;
+
+    if let Some(depth) = matches.value_of("depth") {
+        println!("Value for config depth: {}", depth);
+        search_depth = depth.parse::<i32>().unwrap();
+        println!("set depth: {}", search_depth);
+    }
+
+    let board = matches.value_of("INPUT").unwrap();
+    println!("board: {}", board);
+
+    println!("search depth: {}", search_depth);
 }
