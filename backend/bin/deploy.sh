f3a95ead3a3383d94f1e361330d25ccf8611c3b9
chenyukang <moorekang@gmail.com>
2021-03-29 22:41:11 +0800

    fix function app

--- backend/bin/deploy.sh
+++ backend/bin/deploy.sh
@@ -1,2 +1,3 @@
-cargo build --release --features "server"
-cp target/release/gomoku function/
+rustup target add x86_64-unknown-linux-musl
+cargo build --release --features "server" --target=x86_64-unknown-linux-musl
+cp target/x86_64-unknown-linux-musl/release/gomoku function/
